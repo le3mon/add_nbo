0132e58ceea810311c8203aeb6581a3da893e3be
root <root@localhost.localdomain>
2020-03-30 17:15:15 +0900

    add get_data function

--- main.c
+++ main.c
@@ -3,22 +3,34 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 
+uint32_t get_data(char *arg){
+    FILE *fp;
+    uint32_t tmp;
+    size_t result;
+    if((fp=fopen(arg,"r")) == NULL){
+        printf("Error : can't open file \n");
+        exit(1);
+    }
+    if((result = fread(&tmp,1,sizeof (uint32_t),fp)) != 4){
+        printf("Error : file error \n");
+        exit(1);
+    }
+    fclose(fp);
+    return tmp;
+}
+
 int main(int argc, char *argv[])
 {
-    uint32_t a, b, sum;
-    FILE *fp, *fp2;
+    uint32_t x, y, sum=0;
     if (argc != 3){
         printf("usage : ./add_nbo a.bin b.bin\n");
         exit(1);
     }
-    if (((fp=fopen(argv[1],"r")) == NULL) || ((fp2=fopen(argv[2],"r")) == NULL) ){
-        printf("Error : can't read file \n");
-        exit(1);
-    }
-    fread(&a,1,sizeof (uint32_t),fp);
-    fread(&b,1,sizeof (uint32_t),fp2);
-    a = ntohl(a);
-    b = ntohl(b);
-    sum = a + b;
-    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",a,a, b,b, sum,sum);
+    x = get_data(argv[1]);
+    y = get_data(argv[2]);
+
+    x = ntohl(x);
+    y = ntohl(y);
+    sum = x + y;
+    printf("%d(%#x) + %d(%#x) = %d(%#x)\n",x,x, y,y, sum,sum);
 }
